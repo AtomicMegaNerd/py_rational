1cc3420aed84d6146f2182f7c5832b0185830b22
Chris Dunphy <chris@megaparsec.ca>
2023-02-26 16:07:20 -0700

    The flake is now cross platform

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
+
   description = "This is a program representing rational numbers in Python";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShell.${system} =
-        pkgs.mkShell
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          pkgs.mkShell
           {
             # The packages we need for this project
             buildInputs = with pkgs;
@@ -21,5 +24,5 @@
                 ruff
               ];
           };
-    };
+      });
 }
