e762ab5febd5255d85c3360cec657953a28762b3
Chris Dunphy <chris@megaparsec.ca>
2024-06-04 14:19:40 -0600

    Use nixpkgs for this flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
   description = "This is a program representing rational numbers in Python";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
